4fd211113cea8b604479560169e2ba39dbef00dd
puerarongo <akhardin96@gmail.com>
2023-07-11 19:50:12 +0300

    add 404.html

--- src/index.tsx
+++ src/index.tsx
@@ -17,7 +17,7 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/">
+        <BrowserRouter basename="/tweets">
           <App />
         </BrowserRouter>
       </PersistGate>
